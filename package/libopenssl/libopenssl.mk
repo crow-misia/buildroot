6cbfe9b7f986cade7c1a49f2d5d0099dd4374932
Zenichi Amano <crow.misia@gmail.com>
2018-12-04 02:18:33 +0900

    package/libopenssl: bump version to 1.1.0j
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- package/libopenssl/libopenssl.mk
+++ package/libopenssl/libopenssl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBOPENSSL_VERSION = 1.1.0h
+LIBOPENSSL_VERSION = 1.1.0j
 LIBOPENSSL_SITE = https://www.openssl.org/source
 LIBOPENSSL_SOURCE = openssl-$(LIBOPENSSL_VERSION).tar.gz
 LIBOPENSSL_LICENSE = OpenSSL or SSLeay
