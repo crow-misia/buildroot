40d226eb74dd6b236d86203919dce3ff01202416
Zenichi Amano <crow.misia@gmail.com>
2019-09-10 20:02:01 +0900

    package/depot-tools: new package
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

---
+++ package/depot-tools/depot-tools.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# depot_tools
+#
+################################################################################
+
+DEPOT_TOOLS_VERSION = origin/master
+DEPOT_TOOLS_SITE = https://chromium.googlesource.com/chromium/tools/depot_tools.git
+DEPOT_TOOLS_SITE_METHOD = git
+
+DEPOT_TOOLS_LICENSE = BSD-3-Clause
+DEPOT_TOOLS_LICENSE_FILES = LICENSE
+
+HOST_DEPOT_TOOLS_DEPENDENCIES = host-python
+
+define HOST_DEPOT_TOOLS_INSTALL_CMDS
+	mkdir -p $(HOST_DIR)/depot_tools/
+	cp -dpfr $(@D)/* $(HOST_DIR)/depot_tools/
+endef
+
+$(eval $(host-generic-package))
