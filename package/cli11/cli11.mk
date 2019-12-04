a9def4875f3987bde69f65d62e96f63851f17c54
Zenichi Amano <crow.misia@gmail.com>
2019-12-04 23:49:29 +0900

    package/cli11: new package
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

---
+++ package/cli11/cli11.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# cli11
+#
+################################################################################
+
+CLI11_VERSION = 1.8.0
+CLI11_SITE = $(call github,CLIUtils,CLI11,v$(CLI11_VERSION))
+CLI11_LICENSE = BSD-3
+CLI11_LICENSE_FILES = LICENSE
+CLI11_INSTALL_STAGING = YES
+
+$(eval $(cmake-package))
