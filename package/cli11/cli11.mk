a819429e40b5abbe738bd3318424f1339f9fe6b4
Zenichi Amano <crow.misia@gmail.com>
2020-08-11 07:53:35 +0900

    package/cli11: bump to version 1.9.1
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- package/cli11/cli11.mk
+++ package/cli11/cli11.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CLI11_VERSION = 1.9.0
+CLI11_VERSION = 1.9.1
 CLI11_SITE = $(call github,CLIUtils,CLI11,v$(CLI11_VERSION))
 CLI11_LICENSE = BSD-3
 CLI11_LICENSE_FILES = LICENSE
